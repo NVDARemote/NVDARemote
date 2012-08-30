589d863971613271d1074267f1a911c2527a9f81
Rui Batista <ruiandrebatista@gmail.com>
2012-08-30 16:19:36 +0100

    Add pot target to the template, and better dependency checking. Customize buildVars.py for your own needs.

---
+++ buildVars.py
@@ -0,0 +1,13 @@
+
+# Build customizations
+# Change this file instead of sconstruct, whenever possible.
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = []
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources 
+
