dd593f6fe4687cfc1a70c363174b0681b46959d8
Mesar Hameed <mhameed@src.gnome.org>
2013-10-12 15:20:05 +0200

    another fix for guidelines url, hopefully this should be a stable url now.

--- buildVars.py
+++ buildVars.py
@@ -9,7 +9,7 @@ _ = lambda x : x
 # Add-on information variables
 addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
-	# https://bitbucket.org/nvdaaddonteam/todo/src/37bd08d42c17e72ae303fee4a60821ea0c2f4c5a/guideLines.txt?at=master
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
 	"addon-name" : "addonTemplate",
 	# Add-on summary, usually the user visible name of the addon.
