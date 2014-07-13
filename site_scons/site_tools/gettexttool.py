a6005be4f34ad48b85cb3b878c4ae0c97cf10b8a
Rui Batista <ruiandrebatista@gmail.com>
2014-07-13 17:48:19 +0100

    Simplify gettext tool with more variable interpolation.

--- site_scons/site_tools/gettexttool.py
+++ site_scons/site_tools/gettexttool.py
@@ -15,7 +15,7 @@ To properly configure get text, define the following variables:
 
 
 """
-
+from SCons.Action import Action
 
 def exists(env):
 	return True
@@ -26,18 +26,18 @@ def generate(env):
 	env.SetDefault(gettext_package_version="")
 
 	env['BUILDERS']['gettextMoFile']=env.Builder(
-		action=env.Action(["msgfmt -o $TARGETS $SOURCES"], lambda t, s, e : "Compiling translation %s" % s[0]),
+		action=Action("msgfmt -o $TARGET $SOURCE", "Compiling translation $SOURCE"),
 		suffix=".mo",
 		src_suffix=".po"
 	)
 
 	env['BUILDERS']['gettextPotFile']=env.Builder(
-		action=env.Action(["xgettext --msgid-bugs-address='$gettext_package_bugs_address' --package-name='$gettext_package_name' --package-version='$gettext_package_version' -c -o $TARGETS $SOURCES"],
-		lambda t, s, e : "Generating pot file %s" % t[0]),
+		action=Action("xgettext --msgid-bugs-address='$gettext_package_bugs_address' --package-name='$gettext_package_name' --package-version='$gettext_package_version' -c -o $TARGET $SOURCE",
+		"Generating pot file $TARGET"),
 		suffix=".pot")
 
 	env['BUILDERS']['gettextMergePotFile']=env.Builder(
-		action=env.Action(["xgettext --msgid-bugs-address='$gettext_package_bugs_address' --package-name='$gettext_package_name' --package-version='$gettext_package_version' --omit-header --no-location -c -o $TARGETS $SOURCES"],
-		lambda t, s, e : "Generating pot file %s" % t[0]),
+		action=Action("xgettext --msgid-bugs-address='$gettext_package_bugs_address' --package-name='$gettext_package_name' --package-ve rsion='$gettext_package_version' --omit-header --no-location -c -o $TARGET $OURCES",
+		"Generating pot file %TARGET"),
 		suffix=".pot")
 
