9e86b92dcfa40c8470fc841b0d993aeb870a4458
Christopher Toth <q@q-continuum.net>
2015-07-03 15:51:22 -0600

    Bumped version to 1.01

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
 del /s /q addon\manifest.ini>NUL
-del /s /q "remote-1.0.nvda-addon">NUL
+del /s /q "remote-1.01.nvda-addon">NUL
 call scons -s
-"remote-1.0.nvda-addon"
+"remote-1.01.nvda-addon"
