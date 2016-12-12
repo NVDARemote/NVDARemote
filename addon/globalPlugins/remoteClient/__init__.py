8820cae6144af0c1649c5a9025da7ca3200ed394
Leonard de Ruijter <leonard@babbage.com>
2016-12-12 08:32:28 +0100

    NVDA Remote will no longer unexpectedly autoconnect to a control server on secure screens. Fixes #51

--- addon/globalPlugins/remoteClient/__init__.py
+++ addon/globalPlugins/remoteClient/__init__.py
@@ -67,7 +67,9 @@ class GlobalPlugin(GlobalPlugin):
 		cs = get_config()['controlserver']
 		self.temp_location = os.path.join(shlobj.SHGetFolderPath(0, shlobj.CSIDL_COMMON_APPDATA), 'temp')
 		self.ipc_file = os.path.join(self.temp_location, 'remote.ipc')
-		if not self.check_secure_desktop() and cs['autoconnect']:
+		if globalVars.appArgs.secure:
+			self.handle_secure_desktop()
+		elif cs['autoconnect']:
 			self.perform_autoconnect()
 		self.sd_focused = False
 
@@ -392,9 +394,7 @@ class GlobalPlugin(GlobalPlugin):
 		self.sd_server.close()
 		self.sd_relay.close()
 
-	def check_secure_desktop(self):
-		if not globalVars.appArgs.secure:
-			return False
+	def handle_secure_desktop(self):
 		try:
 			with open(self.ipc_file) as fp:
 				data = json.load(fp)
@@ -405,9 +405,8 @@ class GlobalPlugin(GlobalPlugin):
 			test_socket.connect(('127.0.0.1', port))
 			test_socket.close()
 			self.connect_control(('127.0.0.1', port), channel)
-			return True
 		except:
-			return False
+			pass
 
 	__gestures = {
 		"kb:alt+NVDA+pageDown": "disconnect",
