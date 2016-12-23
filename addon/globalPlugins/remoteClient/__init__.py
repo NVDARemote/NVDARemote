leonardder/unicornNVDA

Braille support for NVDA Remote, based on the new protocol version 2 implmentation. Implemented on behalf of @BabbageCom.
* Braille implementation is hook based, similar to speech and NVWave implementation
* A controlled computer will respect braille settings for multiple controllers and will automatically pick the lowest display size
* Masters will also have braille support on secure screens
* Braille input is supported by sending gesture information and a path to the script to the slave

Closes #68

addon/globalPlugins/remoteClient/__init__.py
@@ -22,6 +22,7 @@ import wx
 import gui
 import beep_sequence
 import speech
+import braille
 from transport import RelayTransport, ConnectorThread
 import local_machine
 import serializer
@@ -343,6 +344,7 @@ class GlobalPlugin(GlobalPlugin):
 			self.key_modified = kwargs['pressed']
 		if kwargs['vk_code'] == win32con.VK_F11 and kwargs['pressed'] and not self.key_modified:
 			self.sending_keys = False
+			self.set_receiving_braille(False)
 			# Translators: Presented when keyboard control is back to the controlling computer.
 			ui.message(_("Not sending keys."))
 			return True #Don't pass it on
@@ -353,6 +355,25 @@ class GlobalPlugin(GlobalPlugin):
 		# Translators: Presented when sending keyboard keys from the controlling computer to the controlled computer.
 		ui.message(_("Sending keys."))
 		self.sending_keys = True
+		self.set_receiving_braille(True)
+
+	def set_receiving_braille(self, state):
+		if state and self.master_session.patch_callbacks_added and braille.handler.enabled:
+			self.master_session.patcher.patch_braille_input()
+			braille.handler.enabled = False
+			if braille.handler._cursorBlinkTimer:
+				braille.handler._cursorBlinkTimer.Stop()
+				braille.handler._cursorBlinkTimer=None
+			if braille.handler.buffer is braille.handler.messageBuffer:
+				braille.handler.buffer.clear()
+				braille.handler.buffer = braille.handler.mainBuffer
+				braille.handler._messageCallLater.Stop()
+				braille.handler._messageCallLater = None
+			self.local_machine.receiving_braille=True
+		elif not state:
+			self.master_session.patcher.unpatch_braille_input()
+			braille.handler.enabled = bool(braille.handler.displaySize)
+			self.local_machine.receiving_braille=False
 
 	def event_gainFocus(self, obj, nextHandler):
 		if isinstance(obj, IAccessibleHandler.SecureDesktopNVDAObject):
@@ -376,7 +397,9 @@ class GlobalPlugin(GlobalPlugin):
 		server_thread = threading.Thread(target=self.sd_server.run)
 		server_thread.daemon = True
 		server_thread.start()
-		self.sd_relay = RelayTransport(address=('127.0.0.1', port), serializer=serializer.JSONSerializer(), channel=channel)
+		self.sd_relay = RelayTransport(address=('127.0.0.1', port), serializer=serializer.JSONSerializer(), channel=channel, connection_type='relay')
+		self.sd_relay.callback_manager.register_callback('msg_client_joined', self.on_master_display_change)
+		self.control_connector.callback_manager.register_callback('msg_set_braille_info', self.on_master_display_change)
 		self.sd_bridge = bridge.BridgeTransport(self.control_connector, self.sd_relay)
 		relay_thread = threading.Thread(target=self.sd_relay.run)
 		relay_thread.daemon = True
@@ -391,6 +414,11 @@ class GlobalPlugin(GlobalPlugin):
 		self.sd_bridge.disconnect()
 		self.sd_server.close()
 		self.sd_relay.close()
+		self.control_connector.callback_manager.unregister_callback('msg_set_braille_info', self.on_master_display_change)
+		self.slave_session.set_display_size()
+
+	def on_master_display_change(self, **kwargs):
+		self.sd_relay.send(type='set_display_size', sizes=self.slave_session.master_display_sizes)
 
 	def check_secure_desktop(self):
 		if not globalVars.appArgs.secure:

NVDARemote/NVDARemote

addon/globalPlugins/remoteClient/__init__.py
@@ -22,6 +22,7 @@ import gui
 import beep_sequence
 import speech
 from transport import RelayTransport
+import braille
 import local_machine
 import serializer
 from session import MasterSession, SlaveSession
@@ -340,6 +341,7 @@ class GlobalPlugin(GlobalPlugin):
 			self.key_modified = kwargs['pressed']
 		if kwargs['vk_code'] == win32con.VK_F11 and kwargs['pressed'] and not self.key_modified:
 			self.sending_keys = False
+			self.set_receiving_braille(False)
 			# Translators: Presented when keyboard control is back to the controlling computer.
 			ui.message(_("Not sending keys."))
 			return True #Don't pass it on
@@ -350,6 +352,25 @@ class GlobalPlugin(GlobalPlugin):
 		# Translators: Presented when sending keyboard keys from the controlling computer to the controlled computer.
 		ui.message(_("Sending keys."))
 		self.sending_keys = True
+		self.set_receiving_braille(True)
+
+	def set_receiving_braille(self, state):
+		if state and self.master_session.patch_callbacks_added and braille.handler.enabled:
+			self.master_session.patcher.patch_braille_input()
+			braille.handler.enabled = False
+			if braille.handler._cursorBlinkTimer:
+				braille.handler._cursorBlinkTimer.Stop()
+				braille.handler._cursorBlinkTimer=None
+			if braille.handler.buffer is braille.handler.messageBuffer:
+				braille.handler.buffer.clear()
+				braille.handler.buffer = braille.handler.mainBuffer
+				braille.handler._messageCallLater.Stop()
+				braille.handler._messageCallLater = None
+			self.local_machine.receiving_braille=True
+		elif not state:
+			self.master_session.patcher.unpatch_braille_input()
+			braille.handler.enabled = bool(braille.handler.displaySize)
+			self.local_machine.receiving_braille=False
 
 	def event_gainFocus(self, obj, nextHandler):
 		if isinstance(obj, IAccessibleHandler.SecureDesktopNVDAObject):
@@ -374,6 +395,8 @@ class GlobalPlugin(GlobalPlugin):
 		server_thread.daemon = True
 		server_thread.start()
 		self.sd_relay = RelayTransport(address=('127.0.0.1', port), serializer=serializer.JSONSerializer(), channel=channel)
+		self.sd_relay.callback_manager.register_callback('msg_client_joined', self.on_master_display_change)
+		self.slave_transport.callback_manager.register_callback('msg_set_braille_info', self.on_master_display_change)
 		self.sd_bridge = bridge.BridgeTransport(self.slave_transport, self.sd_relay)
 		relay_thread = threading.Thread(target=self.sd_relay.run)
 		relay_thread.daemon = True
@@ -391,6 +414,12 @@ class GlobalPlugin(GlobalPlugin):
 		self.sd_server = None
 		self.sd_relay.close()
 		self.sd_relay = None
+		self.slave_transport.callback_manager.unregister_callback('msg_set_braille_info', self.on_master_display_change)
+		self.slave_session.set_display_size()
+
+	def on_master_display_change(self, **kwargs):
+		self.sd_relay.send(type='set_display_size', sizes=self.slave_session.master_display_sizes)
+>>>>>>> Braille support for NVDA Remote, based on the new protocol version 2 implmentation. Implemented on behalf of @BabbageCom.
 
 	def handle_secure_desktop(self):
 		try: