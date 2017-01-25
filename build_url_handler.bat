b39190b797592c0b4eccad6f448c0a1591fe817f
Christopher Toth <q@q-continuum.net>
2017-01-24 21:04:58 -0700

    Build the URL handler with SCons instead of its own build script

--- build_url_handler.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cl /WX /Ox /nologo addon\globalPlugins\remoteClient\url_handler.cpp user32.lib /EHsc /link /subsystem:windows
-move url_handler.exe addon\globalPlugins\remoteClient\ > NUL
