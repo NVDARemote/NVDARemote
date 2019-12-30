4a42652d0da5b2e1c63258aed632187dc49dde73
Reef Turner <reef@nvaccess.org>
2019-12-30 16:42:49 +0100

    Don't use 'unicode()' or 'unichr()'
    Probably need to update the json library.

--- addon/globalPlugins/remoteClient/json/decoder.py
+++ addon/globalPlugins/remoteClient/json/decoder.py
@@ -86,8 +86,8 @@ def py_scanstring(s, end, encoding=None, strict=True,
         content, terminator = chunk.groups()
         # Content is contains zero or more unescaped string characters
         if content:
-            if not isinstance(content, unicode):
-                content = unicode(content, encoding)
+            if not isinstance(content, str):
+                content = str(content, encoding)
             _append(content)
         # Terminator is the end of string, a literal control character,
         # or a backslash denoting that an escape sequence follows
@@ -133,7 +133,7 @@ def py_scanstring(s, end, encoding=None, strict=True,
                 uni2 = int(esc2, 16)
                 uni = 0x10000 + (((uni - 0xd800) << 10) | (uni2 - 0xdc00))
                 next_end += 6
-            char = unichr(uni)
+            char = chr(uni)
             end = next_end
         # Append the unescaped character
         _append(char)
