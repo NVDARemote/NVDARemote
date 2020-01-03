3bd63acbcb57eac40b55f8e70d78c4075c0e636e
Reef Turner <reef@nvaccess.org>
2020-01-03 16:43:30 +0100

    Config spec must be a string

--- addon/globalPlugins/remoteClient/configuration.py
+++ addon/globalPlugins/remoteClient/configuration.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
-from io import BytesIO
+
+from io import StringIO
 import os
 import config
 import configobj
@@ -13,25 +14,25 @@ from . import socket_utils
 CONFIG_FILE_NAME = 'remote.ini'
 
 _config = None
-configspec = BytesIO("""[connections]
-last_connected = list(default=list())
+configspec = StringIO("""
+[connections]
+	last_connected = list(default=list())
 [controlserver]
-autoconnect = boolean(default=False)
-self_hosted = boolean(default=False)
-connection_type = integer(default=0)
-host = string(default="")
-port = integer(default=6837)
-key = string(default="")
+	autoconnect = boolean(default=False)
+	self_hosted = boolean(default=False)
+	connection_type = integer(default=0)
+	host = string(default="")
+	port = integer(default=6837)
+	key = string(default="")
 
 [seen_motds]
-__many__ = string(default="")
-    
+	__many__ = string(default="")
 """)
 def get_config():
 	global _config
 	if not _config:
-		path = os.path.join(globalVars.appArgs.configPath, CONFIG_FILE_NAME)
-		_config = configobj.ConfigObj(path, configspec=configspec)
+		path = os.path.abspath(os.path.join(globalVars.appArgs.configPath, CONFIG_FILE_NAME))
+		_config = configobj.ConfigObj(infile=path, configspec=configspec, create_empty=True)
 		val = validate.Validator()
 		_config.validate(val, copy=True)
 	return _config
