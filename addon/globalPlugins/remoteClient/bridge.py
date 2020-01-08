778569357c4097de4596a90fc27b970f7982ae45
Reef Turner <reef@nvaccess.org>
2020-01-08 16:58:40 +0100

    Remove future imports
    This addon will only be compatible with Python 3.7

--- addon/globalPlugins/remoteClient/bridge.py
+++ addon/globalPlugins/remoteClient/bridge.py
@@ -1,6 +1,5 @@
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
+
+
 class BridgeTransport(object):
 	"""Object to bridge two transports together,
 	passing messages to both of them.
