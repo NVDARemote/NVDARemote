1958454b58019cfd4fae61f8810d58156ef837c2
Reef Turner <reef@nvaccess.org>
2020-01-14 18:39:08 +0100

    Fixup: Use built in JSON from python 3 lib
    Missed a couple of sys.path modifications required for prior json library.

--- addon/globalPlugins/remoteClient/dialogs.py
+++ addon/globalPlugins/remoteClient/dialogs.py
@@ -1,8 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 import json
-sys.path.remove(sys.path[-1])
 import random
 import threading
 from urllib import request
