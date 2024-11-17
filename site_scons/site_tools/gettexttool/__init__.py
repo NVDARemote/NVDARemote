ca84e5aa1dd4bdc2b9615296c7dbffaa7dff29a5
Tyler Spivey <tspivey@pcdesk.net>
2024-11-16 16:31:39 -0800

    Upgrade addon template (#341)
    This upgrades the addon template to commit 2f4ba1ec0ab7eb3a0bbb8fb7a9be0c6459ac09e0.

--- site_scons/site_tools/gettexttool/__init__.py
+++ site_scons/site_tools/gettexttool/__init__.py
@@ -1,4 +1,4 @@
-""" This tool allows generation of gettext .mo compiled files, pot files from source code files
+"""This tool allows generation of gettext .mo compiled files, pot files from source code files
 and pot files for merging.
 
 Three new builders are added into the constructed environment:
@@ -15,6 +15,7 @@ To properly configure get text, define the following variables:
 
 
 """
+
 from SCons.Action import Action
 
 
@@ -36,20 +37,19 @@ def generate(env):
 	env.SetDefault(gettext_package_name="")
 	env.SetDefault(gettext_package_version="")
 
-	env['BUILDERS']['gettextMoFile'] = env.Builder(
+	env["BUILDERS"]["gettextMoFile"] = env.Builder(
 		action=Action("msgfmt -o $TARGET $SOURCE", "Compiling translation $SOURCE"),
 		suffix=".mo",
-		src_suffix=".po"
+		src_suffix=".po",
 	)
 
-	env['BUILDERS']['gettextPotFile'] = env.Builder(
-		action=Action("xgettext " + XGETTEXT_COMMON_ARGS, "Generating pot file $TARGET"),
-		suffix=".pot")
+	env["BUILDERS"]["gettextPotFile"] = env.Builder(
+		action=Action("xgettext " + XGETTEXT_COMMON_ARGS, "Generating pot file $TARGET"), suffix=".pot"
+	)
 
-	env['BUILDERS']['gettextMergePotFile'] = env.Builder(
+	env["BUILDERS"]["gettextMergePotFile"] = env.Builder(
 		action=Action(
-			"xgettext " + "--omit-header --no-location " + XGETTEXT_COMMON_ARGS,
-			"Generating pot file $TARGET"
+			"xgettext " + "--omit-header --no-location " + XGETTEXT_COMMON_ARGS, "Generating pot file $TARGET"
 		),
-		suffix=".pot"
+		suffix=".pot",
 	)
