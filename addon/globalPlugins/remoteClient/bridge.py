8db521d559fc5bc9de3f5650fd5466003a43267b
Christopher Toth <q.alpha@gmail.com>
2024-12-01 18:32:17 -0500

    refactor: Improve callback management in BridgeTransport with type-specific handling

--- addon/globalPlugins/remoteClient/bridge.py
+++ addon/globalPlugins/remoteClient/bridge.py
@@ -1,8 +1,7 @@
-from typing import Any, Set, Union
+from typing import Any, Set, Dict
 from enum import Enum
 from .protocol import RemoteMessageType
 from .transport import Transport
-from functools import partial
 
 class BridgeTransport:
 	excluded: Set[str] = {'client_joined', 'client_left', 'channel_joined', 'set_braille_info'}
@@ -10,21 +9,25 @@ class BridgeTransport:
 	def __init__(self, t1: Transport, t2: Transport) -> None:
 		self.t1 = t1
 		self.t2 = t2
-		# Create and store the bound methods as instance variables
-		self._t1_callback = self.make_callback(self.t1)
-		self._t2_callback = self.make_callback(self.t2)
+		# Store callbacks for each message type
+		self.t1_callbacks: Dict[RemoteMessageType, callable] = {}
+		self.t2_callbacks: Dict[RemoteMessageType, callable] = {}
 		
 		for messageType in RemoteMessageType:
-			t1.registerInbound(messageType, self._t2_callback)
-			t2.registerInbound(messageType, self._t1_callback)
+			# Create and store callbacks
+			self.t1_callbacks[messageType] = self.make_callback(self.t1, messageType)
+			self.t2_callbacks[messageType] = self.make_callback(self.t2, messageType)
+			# Register with stored references
+			t1.registerInbound(messageType, self.t2_callbacks[messageType])
+			t2.registerInbound(messageType, self.t1_callbacks[messageType])
 
-	def make_callback(self, target_transport: Transport):
-		def callback(callback_type, *args, **kwargs):  # clearer that this is the message/callback type
-			if callback_type.value not in self.excluded:
-				target_transport.send(callback_type, *args, **kwargs)
+	def make_callback(self, target_transport: Transport, message_type: RemoteMessageType):
+		def callback(*args, **kwargs):
+			if message_type.value not in self.excluded:
+				target_transport.send(message_type, *args, **kwargs)
 		return callback
-	
+
 	def disconnect(self):
 		for messageType in RemoteMessageType:
-			self.t1.unregisterInbound(messageType, self._t2_callback)
-			self.t2.unregisterInbound(messageType, self._t1_callback)
+			self.t1.unregisterInbound(messageType, self.t2_callbacks[messageType])
+			self.t2.unregisterInbound(messageType, self.t1_callbacks[messageType])
