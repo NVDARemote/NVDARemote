48c4e8053822e70605cdf3c4ac0a15f90c31b7f2
Christopher Toth <q.alpha@gmail.com>
2024-12-01 18:14:18 -0500

    refactor: Simplify BridgeTransport with callback method and instance variables

--- addon/globalPlugins/remoteClient/bridge.py
+++ addon/globalPlugins/remoteClient/bridge.py
@@ -2,36 +2,29 @@ from typing import Any, Set, Union
 from enum import Enum
 from .protocol import RemoteMessageType
 from .transport import Transport
-
+from functools import partial
 
 class BridgeTransport:
-	"""Object to bridge two transports together,
-	passing messages to both of them.
-	We exclude transport-specific messages such as client_joined."""
-	excluded: Set[str] = {'client_joined', 'client_left', 'channel_joined', 'set_braille_info'}
+    excluded: Set[str] = {'client_joined', 'client_left', 'channel_joined', 'set_braille_info'}
 
-	t1: Transport 
-	t2: Transport
+    def __init__(self, t1: Transport, t2: Transport) -> None:
+        self.t1 = t1
+        self.t2 = t2
+        # Create and store the bound methods as instance variables
+        self._t1_callback = self.make_callback(self.t1)
+        self._t2_callback = self.make_callback(self.t2)
+        
+        for messageType in RemoteMessageType:
+            t1.registerInbound(messageType, self._t2_callback)
+            t2.registerInbound(messageType, self._t1_callback)
 
-	def __init__(self, t1: Transport, t2: Transport) -> None:
-		self.t1 = t1
-		self.t2 = t2
-		for messageType in RemoteMessageType:
-			t1.registerInbound(messageType, self.send_to_t2)
-			t2.registerInbound(messageType, self.send_to_t1)
+    def make_callback(self, target_transport: Transport):
+        def callback(message_type, *args, **kwargs):
+            if message_type.value not in self.excluded:
+                target_transport.send(message_type, *args, **kwargs)
+        return callback
 
-	def send(self, transport: Transport, callback: RemoteMessageType, *args: Any, **kwargs: Any) -> None:
-		if callback.value in self.excluded:
-			return
-		transport.send(callback, *args, **kwargs)
-
-	def send_to_t2(self, callback, *args, **kwargs):
-		self.send(self.t2, callback, *args, **kwargs)
-
-	def send_to_t1(self, callback, *args, **kwargs):
-		self.send(self.t1, callback, *args, **kwargs)
-
-	def disconnect(self):
-		for messageType in RemoteMessageType:
-			self.t1.unregisterInbound(messageType, self.send_to_t2)
-			self.t2.unregisterInbound(messageType, self.send_to_t1)
+    def disconnect(self):
+        for messageType in RemoteMessageType:
+            self.t1.unregisterInbound(messageType, self._t2_callback)
+            self.t2.unregisterInbound(messageType, self._t1_callback)
