893f8abeb7b308270b25ff6b916561d2749973b9
David Sexton <david@rustytelephone.net>
2024-12-08 00:01:41 +0530

    Refactor Remote Client Configuration to Use NVDA's Configuration Manager
    - Migrate configuration storage from separate INI file to NVDA's configuration manager
    - Deprecate standalone remote.ini configuration file
    - Update config loading and saving mechanisms to match those expected by NVDA's config manager
    - Correct minor function name and method call adjustments

--- addon/globalPlugins/remoteClient/configuration.py
+++ addon/globalPlugins/remoteClient/configuration.py
@@ -4,12 +4,38 @@ from io import StringIO
 import configobj
 import globalVars
 from configobj import validate
-
+from config import conf as nvdaConf
 from . import socket_utils
 
+configRoot = "Remote"
+# Ensure this config is only in base sections, not sub profiles
+nvdaConf.BASE_ONLY_SECTIONS.add(configRoot)
 CONFIG_FILE_NAME = 'remote.ini'
 
 _config = None
+configspec2 = {
+	'connections': {
+		'last_connected': 'list(default=list())'
+	}, 
+	'controlserver': {
+		'autoconnect': 'boolean(default=False)',
+		'self_hosted': 'boolean(default=False)',
+		'connection_type': 'integer(default=0)', 
+		'host': 'string(default="")', 
+		'port': 'integer(default=6837)', 
+		'key': 'string(default="")'
+	},
+	'seen_motds': {
+		'__many__': 'string(default="")'
+	}, 
+	'trusted_certs': {
+		'__many__': 'string(default="")'
+	}, 
+	'ui': {
+		'play_sounds': 'boolean(default=True)'
+	}
+}
+
 configspec = StringIO("""
 [connections]
 	last_connected = list(default=list())
@@ -30,14 +56,27 @@ configspec = StringIO("""
 [ui]
 	play_sounds = boolean(default=True)
 """)
+
+
 def get_config():
 	global _config
 	if not _config:
+		# Save the config spec to NVDA's config
+		nvdaConf.spec[configRoot] = configspec2
 		path = os.path.abspath(os.path.join(globalVars.appArgs.configPath, CONFIG_FILE_NAME))
-		_config = configobj.ConfigObj(infile=path, configspec=configspec, create_empty=True)
-		val = validate.Validator()
-		_config.validate(val, copy=True)
+		if os.path.exists(path):
+			_config = configobj.ConfigObj(infile=path, configspec=configspec, create_empty=True)
+			val = validate.Validator()
+			_config.validate(val, copy=True)
+			nvdaConf[configRoot] = _config.dict()
+			os.remove(path)
+		save_config()
+	_config = nvdaConf[configRoot]
 	return _config
+
+def save_config():
+	nvdaConf.save()
+	return True
 
 def write_connection_to_config(address):
 	"""Writes an address to the last connected section of the config.
@@ -48,4 +87,4 @@ def write_connection_to_config(address):
 	if address in last_cons:
 		conf['connections']['last_connected'].remove(address)
 	conf['connections']['last_connected'].append(address)
-	conf.write()
+	save_config()
